d448ecfa4accabaa4854a76f521935eb05c1a3b3
SPGoding <SPGoding@outlook.com>
2019-07-16 17:58:26 +0800

    Add an option to change language

--- i18n.js
+++ i18n.js
@@ -1,12 +1,24 @@
+const lngs = [
+  ['en', 'English']
+]
+
+lngs.forEach(v => $('#lngList').append(`<a class="dropdown-item" onclick="changeLng('${v[0]}')">${v[1]}</a>`))
+
 i18next
   .use(i18nextBrowserLanguageDetector)
   .use(i18nextXHRBackend)
   .init({
     backend: { loadPath: 'locales/{{lng}}.json' },
-    whitelist: ['en'],
-    fallbackLng: 'en'
+    fallbackLng: 'en',
+    whitelist: lngs.map(v => v[0])
   })
   .then(() => {
     jqueryI18next.init(i18next, $, { parseDefaultValueFromContent: false })
     $('html').localize()
   })
+
+function changeLng(code) {
+  i18next.changeLanguage(code).then(() => {
+    $('html').localize()
+  })
+}
