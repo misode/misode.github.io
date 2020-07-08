6513ea73423064ad5809aede66638a7be4a997ef
SPGoding <SPGoding@outlook.com>
2020-07-08 15:01:22 -0500

    Fix crashing when fetching 404 registry

--- src/app/RegistryFetcher.ts
+++ src/app/RegistryFetcher.ts
@@ -1,11 +1,15 @@
 import { CollectionRegistry } from '@mcschema/core'
 
-export const mcdata = (version: string, registry: string) => `https://raw.githubusercontent.com/Arcensoth/mcdata/${version}/processed/reports/registries/${registry}/${registry}.min.json`
+export const mcdata = (version: string, registry: string) => `https://raw.githubusercontent.com/Arcensoth/mcdata/${version}/processed/reports/registries/${registry}/${registry.split('/').slice(-1)[0]}.min.json`
 
 export const RegistryFetcher = async (target: CollectionRegistry, registries: string[], version = 'master') => {
   await Promise.all(registries.map(async r => {
-    const res = await fetch(mcdata(version, r))
-    const data = await res.json()
-    target.register(r, data.values)
+    try {
+      const res = await fetch(mcdata(version, r))
+      const data = await res.json()
+      target.register(r, data.values)
+    } catch (e) {
+      console.error(`Error occurred while fetching registry for ${r}.`, e)
+    }
   }))
 }
