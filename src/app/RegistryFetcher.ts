a175976c48f7b6b4a30cbfb0e25f7047f8e3105c
Misode <Misoloo64@gmail.com>
2020-08-04 00:55:17 +0200

    Fix mcdata registry url

--- src/app/RegistryFetcher.ts
+++ src/app/RegistryFetcher.ts
@@ -1,8 +1,8 @@
 import { CollectionRegistry } from '@mcschema/core'
 
 export const mcdata = (version: string, registry: string) => {
-  return `https://raw.githubusercontent.com/Arcensoth/mcdata/${version}/processed/reports/registries/${registry}/${registry.split('/').pop()}.min.json`
-}  
+  return `https://raw.githubusercontent.com/Arcensoth/mcdata/${version}/processed/reports/registries/${registry}/data.min.json`
+}
 
 export const RegistryFetcher = async (target: CollectionRegistry, registries: string[], version = 'master') => {
   await Promise.all(registries.map(async r => {
