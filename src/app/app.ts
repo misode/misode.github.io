946f7e4683c1238eec486d4eabc8836d3d9ac3f1
Misode <Misoloo64@gmail.com>
2020-08-15 01:27:12 +0200

    Minor changes to model picker

--- src/app/app.ts
+++ src/app/app.ts
@@ -344,7 +344,7 @@ Promise.all([
         output.insertAdjacentHTML('beforeend', 
           `<div class="generators-card${m.id === selected ? ' selected' : ''}">
             ${locale(m.name)}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"></path></svg>
+            ${m.schema ? '' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"></path></svg>'}
           </div>`)
           output.lastChild?.addEventListener('click', evt => {
           reload(publicPath + m.id)
