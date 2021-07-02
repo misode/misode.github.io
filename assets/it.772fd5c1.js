c4a0659c763256434acf531c018c1d43ccddcf90
misode <Misoloo64@gmail.com>
2021-07-02 00:36:58 +0000

    Deploying to gh-pages from  @ f43cb03cc3ba4604b3f740046ed098bc534f45cf 🚀

--- assets/it.772fd5c1.js
+++
@@ -1,2 +0,0 @@
-var i=JSON.parse('{"advancement":"Avanzamento","copy":"Copia","dimension":"Dimensione","dimension-type":"Tipo di Dimensione","download":"Scarica","fields":"Campi","item-modifier":"Modificatore d\'Oggetti","language":"Lingua","loot-table":"Tavola del Bottino","predicate":"Predica","redo":"Rifai"}');export default i;
-//# sourceMappingURL=it.772fd5c1.js.map
---
+++ assets/it.fd9a8268.js
@@ -0,0 +1,2 @@
+var i=JSON.parse('{"advancement":"Avanzamento","copy":"Copia","dimension":"Dimensione","dimension_type":"Tipo di Dimensione","download":"Scarica","fields":"Campi","item_modifier":"Modificatore d\'Oggetti","language":"Lingua","loot_table":"Tavola del Bottino","predicate":"Predica","redo":"Rifai"}');export default i;
+//# sourceMappingURL=it.fd9a8268.js.map
