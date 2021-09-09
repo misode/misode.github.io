14d176dafe905a86131237542bd1a14ed2d3daa6
Misode <Misoloo64@gmail.com>
2021-09-09 21:28:29 +0200

    More changes to renderHtml

--- src/app/components/Tree.tsx
+++ src/app/components/Tree.tsx
@@ -1,10 +1,12 @@
 import type { DataModel } from '@mcschema/core'
 import { ModelPath } from '@mcschema/core'
-import { useErrorBoundary, useState } from 'preact/hooks'
+import type { JSX } from 'preact'
+import { useErrorBoundary, useMemo, useRef, useState } from 'preact/hooks'
+import rfdc from 'rfdc'
 import { useModel } from '../hooks'
-import { locale } from '../Locales'
 import { renderHtml } from '../schema/renderHtml'
 import type { BlockStateRegistry, VersionId } from '../Schemas'
+const clone = rfdc()
 
 type TreePanelProps = {
 	lang: string,
@@ -13,27 +15,29 @@ type TreePanelProps = {
 	blockStates: BlockStateRegistry | null,
 	onError: (message: string) => unknown,
 }
-export function Tree({ lang, model, version, blockStates, onError }: TreePanelProps) {
+export function Tree({ lang, model, blockStates, onError }: TreePanelProps) {
 	if (!model || !blockStates) return <></>
 
-	useErrorBoundary(e => {
+	const [error] = useErrorBoundary(e => {
 		onError(`Error rendering the tree: ${e.message}`)
 		console.error(e)
 	})
+	if (error) return <></>
 
-	const [, setState] = useState(0)
+	const [state, setState] = useState(0)
 	useModel(model, () => {
 		setState(state => state + 1)
 	})
 
-	const props = { loc: locale.bind(null, lang), version, blockStates }
 	const path = new ModelPath(model)
-	const [prefix, suffix, body] = model.schema.hook(renderHtml, path, model.data, props)
-
-	return <div class="tree">
-		{suffix ? <div class={`node ${model.schema.type(path)}-node`} data-category={model.schema.category(path)}>
+	const tree = useRef<JSX.Element | null>(null)
+	useMemo(() => {
+		const [prefix, suffix, body] = model.schema.hook(renderHtml, path, clone(model.data), lang, blockStates)
+		tree.current = suffix?.props?.children.some((c: any) => c) ? <div class={`node ${model.schema.type(path)}-node`} data-category={model.schema.category(path)}>
 			<div class="node-header">{prefix}{suffix}</div>
 			<div class="node-body">{body}</div>
-		</div> : body}
-	</div>
+		</div> : body
+	}, [lang, model, blockStates, state])
+
+	return <div class="tree">{tree.current}</div>
 }
--- src/app/schema/renderHtml.tsx
+++ src/app/schema/renderHtml.tsx
@@ -1,17 +1,11 @@
-import type { EnumOption, Hook, ValidationOption } from '@mcschema/core'
+import type { BooleanHookParams, EnumOption, Hook, INode, NumberHookParams, StringHookParams, ValidationOption } from '@mcschema/core'
 import { DataModel, MapNode, ModelPath, ObjectNode, Path, relativePath, StringNode } from '@mcschema/core'
-import type { JSX } from 'preact'
+import type { ComponentChildren, JSX } from 'preact'
 import { Octicon } from '../components/Octicon'
 import { useFocus } from '../hooks'
-import type { Localize } from '../Locales'
-import type { BlockStateRegistry, VersionId } from '../Schemas'
+import { locale } from '../Locales'
+import type { BlockStateRegistry } from '../Schemas'
 import { hexId } from '../Utils'
-
-export type TreeProps = {
-	loc: Localize,
-	version: VersionId,
-	blockStates: BlockStateRegistry,
-}
 
 const selectRegistries = ['loot_table.type', 'loot_entry.type', 'function.function', 'condition.condition', 'criterion.trigger', 'dimension.generator.type', 'dimension.generator.biome_source.type', 'carver.type', 'feature.type', 'decorator.type', 'feature.tree.minimum_size.type', 'block_state_provider.type', 'trunk_placer.type', 'foliage_placer.type', 'tree_decorator.type', 'int_provider.type', 'float_provider.type', 'height_provider.type', 'structure_feature.type', 'surface_builder.type', 'processor.processor_type', 'rule_test.predicate_type', 'pos_rule_test.predicate_type', 'template_element.element_type', 'block_placer.type']
 const hiddenFields = ['number_provider.type', 'score_provider.type', 'nbt_provider.type', 'int_provider.type', 'float_provider.type', 'height_provider.type']
@@ -28,7 +22,9 @@ const keysModel = new DataModel(MapNode(
 ), { historyMax: 0 })
 
 type JSXTriple = [JSX.Element | null, JSX.Element | null, JSX.Element | null]
-type RenderHook = Hook<[any, TreeProps], JSXTriple>
+type RenderHook = Hook<[any, string, BlockStateRegistry], JSXTriple>
+
+type NodeProps<T> = T & { node: INode<any> } & { path: ModelPath } & { value: any} & { lang: string } & { states: BlockStateRegistry }
 
 /**
  * Renders the node and handles events to update the model
@@ -39,38 +35,32 @@ export const renderHtml: RenderHook = {
 		return [null, null, null]
 	},
 
-	boolean({ node }, path, value, props) {
-		const set = (target: boolean) => {
-			path.model.set(path, node.optional() && value === target ? undefined : target)
-		}
-		const suffix = <>
-			<button class={value === false ? 'selected' : ''} onClick={() => set(false)}>{props.loc('false')}</button>
-			<button class={value === true ? 'selected' : ''} onClick={() => set(true)}>{props.loc('true')}</button>
-		</>
-		return [null, suffix, null]
+	boolean(params, path, value, lang, states) {
+		return [null, <BooleanSuffix {...{...params, path, value, lang, states}} />, null]
 	},
 
-	choice({ choices, config, switchNode }, path, value, props) {
+	choice({ choices, config, switchNode }, path, value, lang, states) {
 		const choice = switchNode.activeCase(path, true) as typeof choices[number]
 		const contextPath = (config?.context) ? new ModelPath(path.getModel(), new Path(path.getArray(), [config.context])) : path
-		const [prefix, suffix, body] = choice.node.hook(this, contextPath, value, props)
+		const [prefix, suffix, body] = choice.node.hook(this, contextPath, value, lang, states)
 		if (choices.length === 1) {
 			return [prefix, suffix, body]
 		}
 		const choiceContextPath = config?.choiceContext ? new Path([], [config.choiceContext]) : config?.context ? new Path([], [config.context]) : path
 		const set = (value: string) => {
 			const c = choices.find(c => c.type === value) ?? choice
+			console.log(c)
 			path.model.set(path, c.change ? c.change(value) : c.node.default())
 		}
 		const inject = <select value={choice.type} onChange={(e) => set((e.target as HTMLSelectElement).value)}>
 			{choices.map(c => <option value={c.type}>
-				{pathLocale(props.loc, choiceContextPath.contextPush(c.type))}
+				{pathLocale(lang, choiceContextPath.contextPush(c.type))}
 			</option>)}
 		</select>
 		return [prefix, <>{inject}{suffix}</>, body]
 	},
 
-	list({ children }, path, value, props) {
+	list({ children }, path, value, lang, states) {
 		const onAdd = () => {
 			if (!Array.isArray(value)) value = []
 			path.model.set(path, [children.default(), ...value])
@@ -81,34 +71,27 @@ export const renderHtml: RenderHook = {
 		}
 		const suffix = <button class="add" onClick={onAdd}>{Octicon.plus_circle}</button>
 		const body = <>
-			{(value && Array.isArray(value)) && value.map((child, index) => {
+			{(value && Array.isArray(value)) && value.map((cValue, index) => {
 				const cPath = path.push(index).contextPush('entry')
-				const [cPrefix, cSuffix, cBody] = children.hook(this, cPath, child, props)
 				const onRemove = () => cPath.set(undefined)
 				const onMoveUp = () => {
-					[value[index - 1], value[index]] = [value[index], value[index - 1]]
-					path.model.set(path, value)
+					const v = [...value];
+					[v[index - 1], v[index]] = [v[index], v[index - 1]]
+					path.model.set(path, v)
 				}
 				const onMoveDown = () => {
-					[value[index + 1], value[index]] = [value[index], value[index + 1]]
-					path.model.set(path, value)
+					const v = [...value];
+					[v[index + 1], v[index]] = [v[index], v[index + 1]]
+					path.model.set(path, v)
 				}
 				return <div class="node-entry">
-					<div class={`node ${children.type(cPath)}-node`} data-category={children.category(cPath)}>
-						<div class="node-header">
-							<ErrorPopup loc={props.loc} path={cPath} />
-							<HelpPopup loc={props.loc} path={cPath} />
-							<button class="remove" onClick={onRemove}>{Octicon.trashcan}</button>
-							{value.length > 1 && <div class="node-move">
-								<button class="move" onClick={onMoveUp} disabled={index === 0}>{Octicon.chevron_up}</button>
-								<button class="move" onClick={onMoveDown} disabled={index === value.length - 1}>{Octicon.chevron_down}</button>
-							</div>}
-							{cPrefix}
-							<label>{pathLocale(props.loc, cPath, `${index}`)}</label>
-							{cSuffix}
-						</div>
-						{cBody && <div class="node-body">{cBody}</div>}
-					</div>
+					<TreeNode path={cPath} schema={children} value={cValue} lang={lang} states={states}>
+						<button class="remove" onClick={onRemove}>{Octicon.trashcan}</button>
+						{value.length > 1 && <div class="node-move">
+							<button class="move" onClick={onMoveUp} disabled={index === 0}>{Octicon.chevron_up}</button>
+							<button class="move" onClick={onMoveDown} disabled={index === value.length - 1}>{Octicon.chevron_down}</button>
+						</div>}
+					</TreeNode>
 				</div>
 			})}
 			{(value && value.length > 2) && <div class="node-entry">
@@ -120,13 +103,13 @@ export const renderHtml: RenderHook = {
 		return [null, suffix, body]
 	},
 
-	map({ children, keys, config }, path, value, props) {
+	map({ children, keys, config }, path, value, lang, states) {
 		const keyPath = new ModelPath(keysModel, new Path([hashString(path.toString())]))
 		const onAdd = () => {
 			const key = keyPath.get()
 			path.model.set(path.push(key), children.default())
 		}
-		const blockState = config.validation?.validator === 'block_state_map'? props.blockStates?.[relativePath(path, config.validation.params.id).get()] : null
+		const blockState = config.validation?.validator === 'block_state_map' ? states?.[relativePath(path, config.validation.params.id).get()] : null
 		const keysSchema = blockState?.properties
 			? StringNode(null!, { enum: Object.keys(blockState.properties ?? {}) })
 			: keys
@@ -141,64 +124,47 @@ export const renderHtml: RenderHook = {
 					path.model.errors.add(path.push(key), 'error.invalid_enum_option', value[key])
 				}
 			})
-			return ObjectNode(Object.fromEntries(properties)).hook(this, path, value, props)
+			return ObjectNode(Object.fromEntries(properties)).hook(this, path, value, lang, states)
 		}
 		const suffix = <>
-			{keysSchema.hook(this, keyPath, keyPath.get() ?? '', props)[1]}
+			{keysSchema.hook(this, keyPath, keyPath.get() ?? '', lang, states)[1]}
 			<button class="add" onClick={onAdd}>{Octicon.plus_circle}</button>
 		</>
 		const body = <>
-			{typeof value === 'object' && Object.keys(value).map(key => {
+			{typeof value === 'object' && Object.entries(value).map(([key, cValue]) => {
 				const cPath = path.modelPush(key)
 				const cSchema = blockState
 					? StringNode(null!, { enum: blockState.properties?.[key] ?? [] })
 					: children
-				if (blockState?.properties?.[key] && !blockState.properties?.[key].includes(value[key])) {
-					path.model.errors.add(cPath, 'error.invalid_enum_option', value[key])
+				if (blockState?.properties?.[key] && typeof cValue === 'string'
+					&& !blockState.properties?.[key].includes(cValue)) {
+					path.model.errors.add(cPath, 'error.invalid_enum_option', cValue)
 				}
-				const [cPrefix, cSuffix, cBody] = cSchema.hook(this, cPath, value[key], props)
 				const onRemove = () => cPath.set(undefined)
-				return <div class="node-entry">
-					<div class={`node ${children.type(cPath)}-node`} data-category={children.category(cPath)}>
-						<div class="node-header">
-							<ErrorPopup loc={props.loc} path={cPath} />
-							<HelpPopup loc={props.loc} path={cPath} />
-							<button class="remove" onClick={onRemove}>{Octicon.trashcan}</button>
-							{cPrefix}
-							<label>{key}</label>
-							{cSuffix}
-						</div>
-						{cBody && <div class="node-body">{cBody}</div>}
-					</div>
+				return <div class="node-entry" key={key}>
+					<TreeNode schema={cSchema} path={cPath} value={cValue} lang={lang} states={states} label={key}>
+						<button class="remove" onClick={onRemove}>{Octicon.trashcan}</button>
+					</TreeNode>
 				</div>
 			})}
 		</>
 		return [null, suffix, body]
 	},
 
-	number({ integer, config }, path, value) {
-		const onChange = (evt: Event) => {
-			const value = (evt.target as HTMLInputElement).value
-			const parsed = config?.color
-				? parseInt(value.slice(1), 16)
-				: integer ? parseInt(value) : parseFloat(value)
-			path.model.set(path, parsed)
-		}
-		const suffix = <input type={config?.color ? 'color' : 'text'} onChange={onChange}
-			value={config?.color ? '#' + value?.toString(16).padStart(6, '0') ?? '#000000' : value ?? ''} />
-		return [null, suffix, null]
+	number(params, path, value, lang, states) {
+		return [null, <NumberSuffix {...{...params, path, value, lang, states}} />, null]
 	},
 
-	object({ node, getActiveFields, getChildModelPath }, path, value, props) {
+	object({ node, getActiveFields, getChildModelPath }, path, value, lang, states) {
 		let prefix: JSX.Element | null = null
 		let suffix: JSX.Element | null = null
 		if (node.optional()) {
 			if (value === undefined) {
 				const onExpand = () => path.set(node.default())
-				suffix = <button class="collapse closed" onClick={onExpand}>${Octicon.plus_circle}</button>
+				suffix = <button class="collapse closed" onClick={onExpand}>{Octicon.plus_circle}</button>
 			} else {
 				const onCollapse = () => path.set(undefined)
-				suffix = <button class="collapse open" onClick={onCollapse}>${Octicon.trashcan}</button>
+				suffix = <button class="collapse open" onClick={onCollapse}>{Octicon.trashcan}</button>
 			}
 		}
 		const body = <>
@@ -209,74 +175,115 @@ export const renderHtml: RenderHook = {
 						const cPath = getChildModelPath(path, key)
 						const context = cPath.getContext().join('.')
 						if (hiddenFields.includes(context)) return null
-						const [cPrefix, cSuffix, cBody] = child.hook(this, cPath, value[key], props)
-						if (!cPrefix && !cSuffix && !cBody) return null
+						const [cPrefix, cSuffix, cBody] = child.hook(this, cPath, value[key], lang, states)
+						if (!cPrefix && !cSuffix && !((cBody?.props?.children?.length ?? 0) > 0)) return null
 						const isFlattened = child.type(cPath) === 'object' && flattenedFields.includes(context)
 						const isInlined = inlineFields.includes(context)
 						if (isFlattened || isInlined) {
-							prefix = <>{prefix}<ErrorPopup loc={props.loc} path={cPath} /><HelpPopup loc={props.loc} path={cPath} />{cPrefix}</>
+							prefix = <>{prefix}<ErrorPopup lang={lang} path={cPath} /><HelpPopup lang={lang} path={cPath} />{cPrefix}</>
 							suffix = <>{suffix}{cSuffix}</>
 							return isFlattened ? cBody : null
 						}
-						return <div class={`node ${child.type(cPath)}-node${cBody ? '' : ' no-body'}`}
-							data-category={child.category(cPath)}>
-							<div class="node-header">
-								<ErrorPopup loc={props.loc} path={cPath} />
-								<HelpPopup loc={props.loc} path={cPath} />
-								{cPrefix}
-								<label>{pathLocale(props.loc, cPath)}</label>
-								{cSuffix}
-							</div>
-							{cBody && <div class="node-body">{cBody}</div>}
-						</div>
+						return <TreeNode schema={child} path={cPath} value={value[key]} lang={lang} states={states} />
 					})
 			}
 		</>
 		return [prefix, suffix, body]
 	},
 
-	string({ node, getValues, config }, path, value, props) {
-		const onChange = (evt: Event) => {
-			const newValue = (evt.target as HTMLSelectElement).value
-			path.model.set(path, newValue.length === 0 ? undefined : newValue)
-			evt.stopPropagation()
-		}
-		let suffix = null
-		const values = getValues()
-		const context = path.getContext().join('.')
-		if (nbtFields.includes(context)) {
-			suffix = <textarea value={value ?? ''} onChange={onChange}></textarea>
-		} else if ((isEnum(config) && !config.additional) || selectRegistries.includes(context)) {
-			let context = new Path([])
-			if (isEnum(config) && typeof config.enum === 'string') {
-				context = context.contextPush(config.enum)
-			} else if (!isEnum(config) && config?.validator === 'resource' && typeof config.params.pool === 'string') {
-				context = context.contextPush(config.params.pool)
-			}
-			suffix = <select value={value ?? ''} onChange={onChange}>
-				{node.optional() && <option value="">{props.loc('unset')}</option>}
-				{values.map(v => <option value={v}>
-					{pathLocale(props.loc, context.contextPush(v.replace(/^minecraft:/, '')))}
-				</option>)}
-			</select>
-		} else if (!isEnum(config) && config?.validator === 'block_state_key') {
-			const blockState = props.blockStates?.[relativePath(path, config.params.id).get()]
-			const values = Object.keys(blockState?.properties ?? {})
-			suffix = <select value={value ?? ''} onChange={onChange}>
-				{values.map(v => `<option>${v}</option>`).join('')}
-			</select>
-		} else {
-			const datalistId = hexId()
-			suffix = <>
-				<input value={value ?? ''} onChange={onChange}
-					list={values.length > 0 ? datalistId : ''} />
-				{values.length > 0 && <datalist id={datalistId}>
-					{values.map(v => <option value={v} />)}
-				</datalist>}
-			</>
-		}
-		return [null, suffix, null]
+	string(params, path, value, lang, states) {
+		return [null, <StringSuffix {...{...params, path, value, lang, states}} />, null]
 	},
+}
+
+function BooleanSuffix({ path, node, value, lang }: NodeProps<BooleanHookParams>) {
+	const set = (target: boolean) => {
+		path.model.set(path, node.optional() && value === target ? undefined : target)
+	}
+	return <>
+		<button class={value === false ? 'selected' : ''} onClick={() => set(false)}>{locale(lang, 'false')}</button>
+		<button class={value === true ? 'selected' : ''} onClick={() => set(true)}>{locale(lang, 'true')}</button>
+	</>
+}
+
+function NumberSuffix({ path, config, integer, value }: NodeProps<NumberHookParams>) {
+	const onChange = (evt: Event) => {
+		const value = (evt.target as HTMLInputElement).value
+		const parsed = config?.color
+			? parseInt(value.slice(1), 16)
+			: integer ? parseInt(value) : parseFloat(value)
+		path.model.set(path, parsed)
+	}
+	return <input type={config?.color ? 'color' : 'text'} onChange={onChange}
+		value={config?.color ? '#' + value?.toString(16).padStart(6, '0') ?? '#000000' : value ?? ''} />
+}
+
+function StringSuffix({ path, getValues, config, node, value, lang, states }: NodeProps<StringHookParams>) {
+	const onChange = (evt: Event) => {
+		const newValue = (evt.target as HTMLSelectElement).value
+		path.model.set(path, newValue.length === 0 ? undefined : newValue)
+		evt.stopPropagation()
+	}
+	const values = getValues()
+	const context = path.getContext().join('.')
+	if (nbtFields.includes(context)) {
+		return <textarea value={value ?? ''} onChange={onChange}></textarea>
+	} else if ((isEnum(config) && !config.additional) || selectRegistries.includes(context)) {
+		let context = new Path([])
+		if (isEnum(config) && typeof config.enum === 'string') {
+			context = context.contextPush(config.enum)
+		} else if (!isEnum(config) && config?.validator === 'resource' && typeof config.params.pool === 'string') {
+			context = context.contextPush(config.params.pool)
+		}
+		return <select value={value ?? ''} onChange={onChange}>
+			{node.optional() && <option value="">{locale(lang, 'unset')}</option>}
+			{values.map(v => <option value={v}>
+				{pathLocale(lang, context.contextPush(v.replace(/^minecraft:/, '')))}
+			</option>)}
+		</select>
+	} else if (!isEnum(config) && config?.validator === 'block_state_key') {
+		const blockState = states?.[relativePath(path, config.params.id).get()]
+		const values = Object.keys(blockState?.properties ?? {})
+		return <select value={value ?? ''} onChange={onChange}>
+			{values.map(v => <option>{v}</option>)}
+		</select>
+	} else {
+		const datalistId = hexId()
+		return <>
+			<input value={value ?? ''} onChange={onChange}
+				list={values.length > 0 ? datalistId : ''} />
+			{values.length > 0 && <datalist id={datalistId}>
+				{values.map(v => <option value={v} />)}
+			</datalist>}
+		</>
+	}
+}
+
+type TreeNodeProps = {
+	schema: INode<any>,
+	path: ModelPath,
+	value: any,
+	lang: string,
+	states: BlockStateRegistry,
+	compare?: any,
+	label?: string,
+	children?: ComponentChildren,
+}
+function TreeNode({ label, schema, path, value, lang, states, children }: TreeNodeProps) {
+	const type = schema.type(path)
+	const category = schema.category(path)
+	const [prefix, suffix, body] = schema.hook(renderHtml, path, value, lang, states)
+	return <div class={`node ${type}-node`} data-category={category}>
+		<div class="node-header">
+			<ErrorPopup lang={lang} path={path} />
+			<HelpPopup lang={lang} path={path} />
+			{children}
+			{prefix}
+			<label>{label ?? pathLocale(lang, path, `${path.last()}`)}</label>
+			{suffix}
+		</div>
+		{body && <div class="node-body">{body}</div>}
+	</div>
 }
 
 function isEnum(value?: ValidationOption | EnumOption): value is EnumOption {
@@ -293,11 +300,11 @@ function hashString(str: string) {
 	return hash
 }
 
-function pathLocale(loc: Localize, path: Path, ...params: string[]) {
+function pathLocale(lang: string, path: Path, ...params: string[]) {
 	const ctx = path.getContext()
 	for (let i = 0; i < ctx.length; i += 1) {
 		const key = ctx.slice(i).join('.')
-		const result = loc(key, ...params)
+		const result = locale(lang, key, ...params)
 		if (key !== result) {
 			return result
 		}
@@ -305,16 +312,16 @@ function pathLocale(loc: Localize, path: Path, ...params: string[]) {
 	return ctx[ctx.length - 1]
 }
 
-function ErrorPopup({ loc, path }: { loc: Localize, path: ModelPath }) {
+function ErrorPopup({ lang, path }: { lang: string, path: ModelPath }) {
 	const e = path.model.errors.get(path, true)
 	if (e.length === 0) return null
-	const message = e[0].params ? loc(e[0].error, ...e[0].params) : loc(e[0].error)
+	const message = locale(lang, e[0].error, ...(e[0].params ?? []))
 	return popupIcon('node-error', 'issue_opened', message)
 }
 
-function HelpPopup({ loc, path }: { loc: Localize, path: Path }) {
+function HelpPopup({ lang, path }: { lang: string, path: Path }) {
 	const key = path.contextPush('help').getContext().join('.')
-	const message = loc(key)
+	const message = locale(lang, key)
 	if (message === key) return null
 	return popupIcon('node-help', 'info', message)
 }
