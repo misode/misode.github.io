9671f304f77a9638609f6ba073b6f9cf26987066
Misode <Misoloo64@gmail.com>
2021-09-10 23:58:18 +0200

    Fix error when switching generators with previews

--- src/app/components/PreviewPanel.tsx
+++ src/app/components/PreviewPanel.tsx
@@ -1,48 +1,11 @@
 import type { DataModel } from '@mcschema/core'
 import { Path } from '@mcschema/core'
-import type { FunctionalComponent } from 'preact'
 import { useState } from 'preact/hooks'
 import { useModel } from '../hooks'
 import type { VersionId } from '../Schemas'
 import { BiomeSourcePreview, DecoratorPreview, NoiseSettingsPreview } from './previews'
 
 export const HasPreview = ['dimension', 'worldgen/noise_settings', 'worldgen/configured_feature']
-
-export const Previews: {
-	id: string,
-	generator: string,
-	path: Path,
-	predicate: (model: DataModel) => boolean,
-	preview: FunctionalComponent<{
-		lang: string,
-		model: DataModel,
-		data: any,
-		version: VersionId,
-		shown: boolean,
-	}>,
-}[] = [
-	{
-		id: 'biome-noise',
-		generator: 'dimension',
-		path: new Path(['generator', 'biome_source']),
-		predicate: model => model.get(new Path(['generator', 'type'])).endsWith('noise'),
-		preview: BiomeSourcePreview,
-	},
-	{
-		id: 'noise-settings',
-		generator: 'worldgen/noise_settings',
-		path: new Path(['noise']),
-		predicate: () => true,
-		preview: NoiseSettingsPreview,
-	},
-	{
-		id: 'decorator',
-		generator: 'worldgen/configured_feature',
-		path: new Path([]),
-		predicate: () => true,
-		preview: DecoratorPreview,
-	},
-]
 
 type PreviewPanelProps = {
 	lang: string,
@@ -59,13 +22,20 @@ export function PreviewPanel({ lang, model, version, id, shown }: PreviewPanelPr
 		setCount(count => count + 1)
 	})
 
-	return <>
-		{Previews.filter(p => p.generator === id).map(p => {
-			const data = model?.get(p.path)
-			if (!model || data === undefined || !p.predicate(model)) {
-				return <></>
-			}
-			return p.preview({ lang, model: model!, data, version, shown })
-		})}
-	</>
+	if (id === 'dimension' && model?.get(new Path(['generator', 'type']))?.endsWith('noise')) {
+		const data = model.get(new Path(['generator', 'biome_source']))
+		if (data) return <BiomeSourcePreview {...{ lang, model, version, shown, data }} />
+	}
+
+	if (id === 'worldgen/noise_settings' && model) {
+		const data = model.get(new Path(['noise']))
+		if (data) return <NoiseSettingsPreview {...{ lang, model, version, shown, data }} />
+	}
+
+	if (id === 'worldgen/configured_feature' && model) {
+		const data = model.get(new Path([]))
+		if (data) return <DecoratorPreview {...{ lang, model, version, shown, data }} />
+	}
+
+	return <></>
 }
