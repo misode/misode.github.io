d002da81f83b7c3f90bac8d9b6b2b07010c421eb
SPWeblate <73763464+SPWeblate@users.noreply.github.com>
2021-11-23 10:03:15 +0000

    Deploying to gh-pages from  @ 7c4a9b8b8f72976c0150e95b671f10cf4dd3c44b 🚀

--- assets/pt.689bba1d.js
+++
@@ -1,2 +0,0 @@
-var a=JSON.parse('{"copy":"Copiar","indentation.2_spaces":"2 Espaços","indentation.4_spaces":"4 Espaços","indentation.tabs":"Guias","theme.dark":"Escuro","theme.light":"Claro"}');export default a;
-//# sourceMappingURL=pt.689bba1d.js.map
