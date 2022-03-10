e39019d65d3499d8a19560ee7a68834c2ef91168
Misode <Misoloo64@gmail.com>
2022-03-10 23:26:57 +0100

    Define bigint in index.html

--- src/app/FeatureDetection.ts
+++
@@ -1,3 +0,0 @@
-if (typeof BigInt !== 'function') {
-	window.BigInt = (() => 0) as any
-}
--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -3,7 +3,6 @@ import '../styles/global.css'
 import '../styles/nodes.css'
 import { App } from './App'
 import { LocaleProvider, ProjectProvider, ThemeProvider, TitleProvider, VersionProvider } from './contexts'
-import './FeatureDetection'
 
 function Main() {
 	return <LocaleProvider>
