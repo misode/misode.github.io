02e28d4d4b311b00f20823acbbda7b0d4997feb5
Misode <Misoloo64@gmail.com>
2022-10-15 07:23:16 +0200

    Advertise minecraft live bingo

--- src/app/components/Ad.tsx
+++ src/app/components/Ad.tsx
@@ -7,6 +7,24 @@ type AdProps = {
 	id: string,
 }
 export function Ad({ type, id }: AdProps) {
+	if (type === 'text') {
+		return <div data-ea-publisher="misode-github-io" data-ea-type="text" class="ad dark flat loaded">
+			<div class="ea-placement ea-type-text">
+				<a href="https://misode.github.io/bingo/" target="_blank">
+					<div class="ea-content" style="display: flex; gap: 6px; align-items: center;">
+						<img height="36" src="https://misode.github.io/bingo/assets/icon.png" alt="Minecraft Live Bingo logo" />
+						<div class="ea-text">
+							<strong>Bingo!</strong>
+							<span> Generate your own random bingo card to fill out as you watch </span>
+							<strong> Minecraft Live 2022!</strong>
+						</div>
+					</div>
+				</a>
+				<div class="ea-callout"><a rel="nofollow noopener" target="_blank" href="https://github.com/misode">Ad by Misode</a></div>
+			</div>
+		</div>
+	}
+
 	useEffect(() => {
 		document.getElementById('ad-placeholder')?.remove()
 		if ('ethicalads' in window) {
@@ -14,5 +32,5 @@ export function Ad({ type, id }: AdProps) {
 		}
 	}, [])
 
-	return <div data-ea-publisher="misode-github-io" data-ea-type={type} class="ad dark flat" id={id}></div>
+	return <div data-ea-publisher="misode-github-io" data-ea-type={type} class="ad dark flat loaded" id={id}></div>
 }
