61ecaacb416500e2d7f0c37cb36a638a15a0f6e8
fishBone000 <FishBoneEK@outlook.com>
2023-02-26 22:18:02 +0800

    spline drawing in progress

--- src/app/components/SplineCard.tsx
+++ src/app/components/SplineCard.tsx
@@ -1,46 +1,80 @@
-import {useEffect, useRef} from 'preact/hooks'
+import {useEffect, useRef, useState} from 'preact/hooks'
 import {Octicon} from "./Octicon.js";
+import {CubicSpline} from "deepslate";
+import MultiPoint = CubicSpline.MultiPoint;
+import Constant = CubicSpline.Constant;
 
 interface SplinePoint{
 	derivative: number,
 	location: number, 
-	value: Spline
+	value: Spline | number
 }
 
 interface Spline{
 	noise: string,
-	constant?: number,
 	points: SplinePoint[]
 }
 
-/*
 interface Props {
-	spline: Spline,
-	posX?: number,
-	posY?: number,
-	color?: string
+	spline: MultiPoint<number> | Constant
 }
- */
 
-export function SplineCard(){
+
+export function SplineCard({spline}: Props){
 	const canvas = useRef<HTMLCanvasElement>(null)
-	const ctx = useRef<CanvasRenderingContext2D>()
+
+	useEffect(() => {
+		if(!canvas.current)
+			return
+		const ctx = canvas.current.getContext('2d')
+		if(!ctx)
+			return
+		console.log("translating coord sys")
+		ctx.translate(canvas.current.width/2, canvas.current.height/2)
+		ctx.scale(1, -1)
+	}, [canvas])
+
+	const [ minX, setMinX ] = useState(-1)
+	const [ maxX, setMaxX ] = useState(1)
 	
 	useEffect( () => {
-		if(!canvas.current) {
-			console.log("canvas ref is null")
+		console.log("spline changed")
+		console.log(spline)
+		spline.calculateMinMax()
+		if(!canvas.current)
 			return
-		}
-		const ctx2D = canvas.current.getContext('2d')
-		if(!ctx2D) {
-			console.log("failed to get context")
+		const ctx = canvas.current.getContext('2d')
+		if(!ctx)
 			return
+		const width = canvas.current.width
+		const height = canvas.current.height
+		ctx.clearRect(-width/2, -height/2, width, height)
+		ctx.fillStyle = "rgb(255,255,255)";
+		ctx.beginPath()
+		if ((spline instanceof MultiPoint) && spline.locations.length > 1) {
+			setMinX(spline.locations[0])
+			setMaxX(spline.locations[0])
+			for(const location of spline.locations){
+				if(location > maxX)
+					setMaxX(location)
+				if(location < minX)
+					setMinX(location)
+			}
+		} else {
+			setMinX(-1)
+			setMaxX(1)
 		}
-		console.log("drawing rect")
-		ctx.current = ctx2D
-		ctx.current.fillStyle = "rgb(200,0,0)";
-		ctx.current.fillRect (10, 10, 55, 50);
-	}, [])
+
+		console.log("drawing curve")
+		const maxAbs = Math.max(Math.abs(spline.max()), Math.abs(spline.min()))
+		ctx.strokeStyle = "rgb(154,154,154)"
+		console.log(-width/2, spline.compute(minX)/height*maxAbs)
+		ctx.moveTo(-width/2, spline.compute(minX)/height*maxAbs)
+		for(let i = 1; i <= 100; i++){
+			ctx.lineTo(-width/2 + width/100*i, spline.compute(minX + (maxX-minX)/100*i)/height*maxAbs)
+		}
+		ctx.stroke()
+	}, [spline])
 
 	return <div class="spline-card">
 		<div class="spline-drag">{Octicon['code']}
--- src/app/components/previews/SplinePreview.tsx
+++ src/app/components/previews/SplinePreview.tsx
@@ -1,11 +1,42 @@
 import {SplineCard} from "../SplineCard.js";
 import {PreviewProps} from "./index.js";
+import {CubicSpline} from "deepslate";
 
-export const SplinePreview = ({}: PreviewProps) => {
-	console.log("invoke spline preview")
-	return <>
-		<div class="full-preview">
-			<SplineCard/>
-		</div>
-	</>
+function extractor() {
+    return {
+        compute(c: number): number {
+            return c
+        }
+    }
+}
+
+function preProcess(data: any){
+    const spline = {
+    }
+    if('coordinate' in data)
+        spline.coordinate = data.coordinate
+    if(('points' in data) && Array.isArray(data.points)){
+        spline.points = []
+        for(const point of data.points){
+            console.log("point: ", point)
+            if('node' in point && 'location' in point.node && 'derivative' in point.node && 'value' in point.node) {
+                console.log("pushed", point.node)
+                spline.points.push(point.node)
+            }
+        }
+    }
+    console.log("preProcess finish")
+    console.log(spline)
+    return spline
+}
+
+export const SplinePreview = ({data}: PreviewProps) => {
+    console.log("invoke spline preview")
+    console.log(data)
+
+    return <>
+        <div class="full-preview">
+            <SplineCard spline={CubicSpline.fromJson(preProcess(data), extractor)}/>
+        </div>
+    </>
 }
