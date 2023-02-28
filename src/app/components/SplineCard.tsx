8798b740a4e8aa5a9d1a14a9cd8e60c60e0d1f1a
fishBone000 <FishBoneEK@outlook.com>
2023-02-28 15:06:00 +0800

    Add SplineCard resize on edge drag
    Problem: canvas doesn't redraw on resize

--- src/app/components/SplineCard.tsx
+++ src/app/components/SplineCard.tsx
@@ -4,26 +4,22 @@ import {CubicSpline} from "deepslate";
 import MultiPoint = CubicSpline.MultiPoint;
 import Constant = CubicSpline.Constant;
 
-interface SplinePoint {
-    derivative: number,
-    location: number,
-    value: Spline | number
-}
-
-interface Spline {
-    noise: string,
-    points: SplinePoint[]
-}
-
 interface Props {
     spline: MultiPoint<number> | Constant
 }
 
+interface ResizeDirection {
+    width: number
+    height: number
+    posX: number
+    posY: number
+}
 
 export function SplineCard({spline}: Props) {
     const canvas = useRef<HTMLCanvasElement>(null)
     const drag = useRef<HTMLDivElement>(null)
     const card = useRef<HTMLDivElement>(null)
+    const resizeDirection = useRef<ResizeDirection>({width: 1, height: 0, posX: 0, posY: 0})
     // TODO maybe not good practice
     const [ctx, setCtx] = useState<CanvasRenderingContext2D>()
 
@@ -34,43 +30,36 @@ export function SplineCard({spline}: Props) {
             const ctx = canvas.current.getContext('2d')
             if (!ctx)
                 return
-            console.log("translating coord sys", ctx.getTransform())
-            const width = canvas.current.clientWidth
-            const height = canvas.current.clientHeight
-            canvas.current.width = width
-            canvas.current.height = height
-            ctx.translate(width / 2, height / 2)
-            ctx.scale(1, -1)
-            console.log("translation complete", ctx.getTransform())
             setCtx(ctx)
         }
     }, [])
 
-    const onMouseMove = useCallback((e: MouseEvent) => {
+    const onDragMouseMove = useCallback((e: MouseEvent) => {
         if (!card.current)
             return
         card.current.style.left = `${card.current.offsetLeft + e.movementX}px`
         card.current.style.top = `${card.current.offsetTop + e.movementY}px`
     }, [])
 
-    const onMouseUp = useCallback((e: MouseEvent) => {
+    const onDragMouseUp = useCallback((e: MouseEvent) => {
         if (!drag.current)
             return
         if (e.button != 0)
             return
-        document.removeEventListener('mousemove', onMouseMove)
-        document.removeEventListener('mouseup', onMouseUp)
+        document.removeEventListener('mousemove', onDragMouseMove)
+        document.removeEventListener('mouseup', onDragMouseUp)
     }, [])
 
-    const onMouseDown = useCallback((e: MouseEvent) => {
+    const onDragMouseDown = useCallback((e: MouseEvent) => {
         if (!drag.current)
             return
         if (e.button != 0)
             return
-        document.addEventListener('mousemove', onMouseMove)
-        document.addEventListener('mouseup', onMouseUp)
+        document.addEventListener('mousemove', onDragMouseMove)
+        document.addEventListener('mouseup', onDragMouseUp)
     }, [])
 
+    // Draw curve
     useEffect(() => {
         console.log("spline changed")
         console.log(spline)
@@ -79,6 +68,17 @@ export function SplineCard({spline}: Props) {
             return
         if (!ctx)
             return
+
+        console.log("translating coord sys", ctx.getTransform())
+        const width = canvas.current.clientWidth
+        const height = canvas.current.clientHeight
+        canvas.current.width = width
+        canvas.current.height = height
+        ctx.resetTransform()
+        ctx.translate(width / 2, height / 2)
+        ctx.scale(1, -1)
+        console.log("translation complete", ctx.getTransform())
+
         let minX = 0
         let maxX = 0
         // TODO solve situations where all locations have same val
@@ -98,8 +98,6 @@ export function SplineCard({spline}: Props) {
         }
 
         console.log("drawing curve")
-        const width = canvas.current.width
-        const height = canvas.current.height
         console.log('canvas size is: ', [width, height])
         console.log('client size is: ', [canvas.current.clientWidth, canvas.current.clientHeight])
         ctx.clearRect(-width / 2, -height / 2, width, height)
@@ -114,15 +112,52 @@ export function SplineCard({spline}: Props) {
             ctx.lineTo(x, y)
         }
         ctx.stroke()
-    }, [spline])
+    }, [spline, canvas.current?.clientHeight, canvas.current?.clientWidth])
+
+    const buildResizeMouseDownHandler = useCallback((direction: ResizeDirection) => {
+        return (e: MouseEvent) => {
+            if(e.button != 0)
+                return
+            resizeDirection.current = direction
+            document.addEventListener('mousemove', onResizeMouseMove)
+            document.addEventListener('mouseup', onResizeMouseUp)
+        }
+    }, [])
+
+    const onResizeMouseMove = useCallback((e: MouseEvent) => {
+        if (!card.current)
+            return
+        const direction = resizeDirection.current
+        card.current.style.width = `${card.current.clientWidth + e.movementX*direction.width}px`
+        card.current.style.height = `${card.current.clientHeight + e.movementY*direction.height}px`
+        card.current.style.left = `${card.current.offsetLeft + e.movementX*direction.posX}px`
+        card.current.style.top = `${card.current.offsetTop + e.movementY*direction.posY}px`
+    }, [])
+
+    const onResizeMouseUp = useCallback((e: MouseEvent) => {
+        if(e.button != 0)
+            return
+        document.removeEventListener('mousemove', onResizeMouseMove)
+        document.removeEventListener('mousemove', onResizeMouseUp)
+    }, [])
 
     return <div class="spline-card" ref={card}>
-        <div class="spline-drag" ref={drag} onMouseDown={onMouseDown}>{Octicon['code']}</div>
-        <div class="spline-resize" style={{gridArea: 'resize-left'}}/>
-        <canvas class="spline-canvas" ref={canvas} style={{backgroundColor: 'transparent'}}>A canvas.</canvas>
-        <div class="spline-resize" style={{gridArea: 'resize-right'}}/>
-        <div class="spline-resize" style={{gridArea: 'resize-corner-left'}}/>
-        <div class="spline-resize" style={{gridArea: 'resize-bottom'}}/>
-        <div class="spline-resize" style={{gridArea: 'resize-corner-right'}}/>
+        <div class="spline-drag" ref={drag} onMouseDown={onDragMouseDown}>{Octicon['code']}</div>
+        <div class="spline-resize" style={{gridArea: 'resize-left', cursor: 'ew-resize'}}
+             onMouseDown={buildResizeMouseDownHandler({width: -1, height: 0, posX: 1, posY: 0})}
+        />
+        <canvas class="spline-canvas" ref={canvas} style={{backgroundColor: 'transparent'}}/>
+        <div class="spline-resize" style={{gridArea: 'resize-right', cursor: 'ew-resize'}}
+             onMouseDown={buildResizeMouseDownHandler({width: 1, height: 0, posX: 0, posY: 0})}
+        />
+        <div class="spline-resize" style={{gridArea: 'resize-corner-left', cursor: 'nesw-resize'}}
+             onMouseDown={buildResizeMouseDownHandler({width: -1, height: 1, posX: 1, posY: 0})}
+        />
+        <div class="spline-resize" style={{gridArea: 'resize-bottom', cursor: 'ns-resize'}}
+             onMouseDown={buildResizeMouseDownHandler({width: 0, height: 1, posX: 0, posY: 0})}
+        />
+        <div class="spline-resize" style={{gridArea: 'resize-corner-right', cursor: 'nwse-resize'}}
+             onMouseDown={buildResizeMouseDownHandler({width: 1, height: 1, posX: 0, posY: 0})}
+        />
     </div>
 }
