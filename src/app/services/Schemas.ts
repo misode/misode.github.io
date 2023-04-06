47e716df8a1b9f530da1907d658d906fc1a53725
Misode <Misoloo64@gmail.com>
2023-04-07 01:07:51 +0200

    Add 1.20

--- src/app/services/Schemas.ts
+++ src/app/services/Schemas.ts
@@ -5,7 +5,7 @@ import { initPartners } from '../partners/index.js'
 import { message } from '../Utils.js'
 import { fetchData } from './DataFetcher.js'
 
-export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4'] as const
+export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4', '1.20'] as const
 export type VersionId = typeof VersionIds[number]
 
 export const DEFAULT_VERSION: VersionId = '1.19.4'
@@ -48,6 +48,7 @@ const versionGetter: {
 	1.19: () => import('@mcschema/java-1.19'),
 	'1.19.3': () => import('@mcschema/java-1.19.3'),
 	'1.19.4': () => import('@mcschema/java-1.19.4'),
+	'1.20': () => import('@mcschema/java-1.20'),
 }
 
 export let CachedDecorator: INode<any>
