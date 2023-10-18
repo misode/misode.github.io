2396fa0c59f1c9583933f05bca15a7618a5cae37
Apollo <apollo404mc@gmail.com>
2023-10-17 17:21:13 -0700

    Add Lithostitched partner

--- src/app/partners/index.ts
+++ src/app/partners/index.ts
@@ -1,8 +1,11 @@
 import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
 import { initImmersiveWeathering } from './ImmersiveWeathering.js'
+import { initLithostitched } from './Lithostitched.js'
 
 export * from './ImmersiveWeathering.js'
+export * from './Lithostitched.js'
 
 export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry) {
 	initImmersiveWeathering(schemas, collections)
+	initLithostitched(schemas, collections)
 }
