649f14dc0dd0eb5151684243ffd02057c48de9f0
Misode <misoloo64@gmail.com>
2023-12-05 19:28:35 +0100

    Don't show all versions by default + version ranges

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -1,4 +1,5 @@
 import config from '../config.json'
+import type { VersionId } from './services/Schemas.js'
 
 export interface ConfigLanguage {
 	code: string,
@@ -7,9 +8,11 @@ export interface ConfigLanguage {
 }
 
 export interface ConfigVersion {
-	id: string,
+	id: VersionId,
 	pack_format: number,
+	name: string,
 	ref?: string,
+	show?: boolean,
 	dynamic?: boolean,
 }
 
