f855c7d9388fb36a95d0c99bce1005cfe4ad7312
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2024-01-17 19:11:18 -0800

    Add Lithostitched partner (#436)
    * Add Lithostitched partner
    
    * Remove modded biome slice stuff
    
    * Run formatter
    
    ---------
    
    Co-authored-by: Misode <misoloo64@gmail.com>

--- src/app/partners/index.ts
+++ src/app/partners/index.ts
@@ -1,10 +1,13 @@
 import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
 import { initImmersiveWeathering } from './ImmersiveWeathering.js'
+import { initLithostitched } from './Lithostitched.js'
 import { initObsidian } from './Obsidian.js'
 
 export * from './ImmersiveWeathering.js'
+export * from './Lithostitched.js'
 
 export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry) {
 	initImmersiveWeathering(schemas, collections)
+	initLithostitched(schemas, collections)
 	initObsidian(schemas, collections)
 }
