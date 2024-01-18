7c94beb5b292114bd06a4abec812d56e271ccb6e
OliviaTheVampire <sindrefagerheim53@gmail.com>
2024-01-18 03:38:54 +0100

    More support for Obsidian (#233)
    * Start obsidian item generator
    
    * Add part of block generator
    
    * More progress on blocks and items for Obsidian
    
    * Fixed some issues
    
    * Added even more properties for blocks
    
    * fixed this?
    
    * added back these?
    
    * Hopefully fixed this?
    
    * More events
    
    * should fix the issues
    
    * NOW WORK
    
    * adding support for even more
    
    * Fix build
    
    * Rename obsidian IDs
    
    * updated pack version
    
    * Update config
    
    ---------
    
    Co-authored-by: Misode <Misoloo64@gmail.com>

--- src/app/partners/index.ts
+++ src/app/partners/index.ts
@@ -1,8 +1,10 @@
 import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
 import { initImmersiveWeathering } from './ImmersiveWeathering.js'
+import { initObsidian } from './Obsidian.js'
 
 export * from './ImmersiveWeathering.js'
 
 export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry) {
 	initImmersiveWeathering(schemas, collections)
+	initObsidian(schemas, collections)
 }
