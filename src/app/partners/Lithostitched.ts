72f0a6c17b40569c282148c054721df6b64768bd
Misode <misoloo64@gmail.com>
2024-01-18 04:10:50 +0100

    Run formatter

--- src/app/partners/Lithostitched.ts
+++ src/app/partners/Lithostitched.ts
@@ -2,7 +2,7 @@ import type { CollectionRegistry, ResourceType, SchemaRegistry } from '@mcschema
 import { BooleanNode, Case, ChoiceNode, ListNode, Mod, NumberNode, ObjectNode, Opt, Reference as RawReference, StringNode as RawStringNode, Switch } from '@mcschema/core'
 
 
-const ID = "lithostitched"
+const ID = 'lithostitched'
 
 export function initLithostitched(schemas: SchemaRegistry, collections: CollectionRegistry) {
 	const Reference = RawReference.bind(undefined, schemas)
@@ -12,16 +12,16 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 		ChoiceNode(
 			[
 				{
-					type: "string",
+					type: 'string',
 					node: StringNode({
-						validator: "resource",
+						validator: 'resource',
 						params: { pool: id, allowTag: true },
 					}),
 					change: (v: unknown) => {
 						if (
 							Array.isArray(v) &&
-							typeof v[0] === "string" &&
-							!v[0].startsWith("#")
+							typeof v[0] === 'string' &&
+							!v[0].startsWith('#')
 						) {
 							return v[0]
 						}
@@ -29,37 +29,37 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 					},
 				},
 				{
-					type: "list",
+					type: 'list',
 					node: ListNode(
-						StringNode({ validator: "resource", params: { pool: id } })
+						StringNode({ validator: 'resource', params: { pool: id } })
 					),
 					change: (v: unknown) => {
-						if (typeof v === "string" && !v.startsWith("#")) {
+						if (typeof v === 'string' && !v.startsWith('#')) {
 							return [v]
 						}
 						return []
 					},
 				},
 			],
-			{ choiceContext: "tag" }
+			{ choiceContext: 'tag' }
 		)
 
 	// Worldgen Modifiers
 	const MobCategorySpawnSettings = Mod(
 		ObjectNode({
 			type: StringNode({
-				validator: "resource",
-				params: { pool: "entity_type" },
+				validator: 'resource',
+				params: { pool: 'entity_type' },
 			}),
 			weight: NumberNode({ integer: true }),
 			minCount: NumberNode({ integer: true }),
 			maxCount: NumberNode({ integer: true }),
 		}),
 		{
-			category: () => "pool",
+			category: () => 'pool',
 			default: () => [
 				{
-					type: "minecraft:bat",
+					type: 'minecraft:bat',
 					weight: 1,
 				},
 			],
@@ -67,26 +67,26 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 	)
 
 	collections.register(`${ID}:modifier_type`, [
-		"lithostitched:add_biome_spawns",
-		"lithostitched:add_features",
-		"lithostitched:add_structure_set_entries",
-		"lithostitched:add_surface_rule",
-		"lithostitched:add_template_pool_elements",
-		"lithostitched:no_op",
-		"lithostitched:redirect_feature",
-		"lithostitched:remove_biome_spawns",
-		"lithostitched:remove_features",
-		"lithostitched:remove_structures_from_structure_set",
-		"lithostitched:replace_climate",
-		"lithostitched:replace_effects",
+		'lithostitched:add_biome_spawns',
+		'lithostitched:add_features',
+		'lithostitched:add_structure_set_entries',
+		'lithostitched:add_surface_rule',
+		'lithostitched:add_template_pool_elements',
+		'lithostitched:no_op',
+		'lithostitched:redirect_feature',
+		'lithostitched:remove_biome_spawns',
+		'lithostitched:remove_features',
+		'lithostitched:remove_structures_from_structure_set',
+		'lithostitched:replace_climate',
+		'lithostitched:replace_effects',
 	])
 
 	collections.register(`${ID}:modifier_predicate_type`, [
-		"lithostitched:all_of",
-		"lithostitched:any_of",
-		"lithostitched:mod_loaded",
-		"lithostitched:not",
-		"lithostitched:true"
+		'lithostitched:all_of',
+		'lithostitched:any_of',
+		'lithostitched:mod_loaded',
+		'lithostitched:not',
+		'lithostitched:true',
 	])
 
 	schemas.register(`${ID}:worldgen_modifier`, Mod(ObjectNode({
@@ -100,57 +100,57 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 					{
 						type: 'object',
 						node: MobCategorySpawnSettings,
-						change: (v: any) => v[0]
+						change: (v: any) => v[0],
 					},
 					{
 						type: 'list',
 						node: ListNode(MobCategorySpawnSettings),
-						change: (v: any) => Array(v)
-					}
-				])
+						change: (v: any) => Array(v),
+					},
+				]),
 			},
 			'lithostitched:add_features': {
 				biomes: Tag('$worldgen/biome'),
 				features: Tag('$worldgen/configured_feature'),
-				step: StringNode({ enum: 'decoration_step' })
+				step: StringNode({ enum: 'decoration_step' }),
 			},
 			'lithostitched:add_structure_set_entries': {
 				structure_set: StringNode({ validator: 'resource', params: { pool: '$worldgen/structure_set' } }),
 				entries: ListNode(
 					ObjectNode({
 						structure: StringNode({ validator: 'resource', params: { pool: '$worldgen/structure' } }),
-						weight: NumberNode({ integer: true, min: 1 })
+						weight: NumberNode({ integer: true, min: 1 }),
 					})
-				)
+				),
 			},
 			'lithostitched:add_surface_rule': {
-				levels: ListNode(StringNode({ validator: "resource", params: { pool: '$dimension' } })),
-				surface_rule: Reference('material_rule')
+				levels: ListNode(StringNode({ validator: 'resource', params: { pool: '$dimension' } })),
+				surface_rule: Reference('material_rule'),
 			},
 			'lithostitched:add_template_pool_elements': {
 				template_pool: StringNode({ validator: 'resource', params: { pool: '$worldgen/template_pool' } }),
 				elements: ListNode(
 					Reference('template_weighted_element')
-				)
+				),
 			},
 			'lithostitched:redirect_feature': {
 				placed_feature: StringNode({ validator: 'resource', params: { pool: '$worldgen/placed_feature' } }),
-				redirect_to: StringNode({ validator: 'resource', params: { pool: '$worldgen/configured_feature' } })
+				redirect_to: StringNode({ validator: 'resource', params: { pool: '$worldgen/configured_feature' } }),
 			},
 			'lithostitched:remove_biome_spawns': {
 				biomes: Tag('$worldgen/biome'),
-				mobs: Tag('entity_type')
+				mobs: Tag('entity_type'),
 			},
 			'lithostitched:remove_features': {
 				biomes: Tag('$worldgen/biome'),
 				features: Tag('$worldgen/configured_feature'),
-				step: StringNode({ enum: 'decoration_step' })
+				step: StringNode({ enum: 'decoration_step' }),
 			},
 			'lithostitched:remove_structures_from_structure_set': {
 				structure_set: StringNode({ validator: 'resource', params: { pool: '$worldgen/structure_set' } }),
 				structures: ListNode(
 					StringNode({ validator: 'resource', params: { pool: '$worldgen/structure' } })
-				)
+				),
 			},
 			'lithostitched:replace_climate': {
 				biomes: Tag('$worldgen/biome'),
@@ -158,8 +158,8 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 					temperature: NumberNode(),
 					downfall: NumberNode(),
 					has_precipitation: BooleanNode(),
-					temperature_modifier: Opt(StringNode({ enum: ['none', 'frozen'] }))
-				})
+					temperature_modifier: Opt(StringNode({ enum: ['none', 'frozen'] })),
+				}),
 			},
 			'lithostitched:replace_effects': {
 				biomes: Tag('$worldgen/biome'),
@@ -176,34 +176,34 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 						sound: StringNode(),
 						tick_delay: NumberNode({ integer: true }),
 						block_search_extent: NumberNode({ integer: true }),
-						offset: NumberNode()
+						offset: NumberNode(),
 					})),
 					additions_sound: Opt(ObjectNode({
 						sound: StringNode(),
-						tick_chance: NumberNode({ min: 0, max: 1 })
+						tick_chance: NumberNode({ min: 0, max: 1 }),
 					})),
 					music: Opt(ObjectNode({
 						sound: StringNode(),
 						min_delay: NumberNode({ integer: true, min: 0 }),
 						max_delay: NumberNode({ integer: true, min: 0 }),
-						replace_current_music: BooleanNode()
+						replace_current_music: BooleanNode(),
 					})),
 					particle: Opt(ObjectNode({
 						options: ObjectNode({
-							type: StringNode()
+							type: StringNode(),
 						}),
-						probability: NumberNode({ min: 0, max: 1 })
-					}))
-				})
-			}
-		}
+						probability: NumberNode({ min: 0, max: 1 }),
+					})),
+				}),
+			},
+		},
 	}, { context: `${ID}.worldgen_modifier`, disableSwitchContext: true }), {
 		default: () => ({
 			type: `${ID}:add_features`,
 			biomes: '#minecraft:is_overworld',
 			features: 'example:ore_ruby',
-			step: 'underground_ores'
-		})
+			step: 'underground_ores',
+		}),
 	}))
 
 	schemas.register(`${ID}:modifier_predicate`, ObjectNode({
@@ -211,19 +211,19 @@ export function initLithostitched(schemas: SchemaRegistry, collections: Collecti
 		[Switch]: [{ push: 'type' }],
 		[Case]: {
 			'lithostitched:all_of': {
-				predicates: ListNode(Reference(`${ID}:modifier_predicate`))
+				predicates: ListNode(Reference(`${ID}:modifier_predicate`)),
 			},
 			'lithostitched:any_of': {
-				predicates: ListNode(Reference(`${ID}:modifier_predicate`))
+				predicates: ListNode(Reference(`${ID}:modifier_predicate`)),
 			},
 			'lithostitched:mod_loaded': {
-				mod_id: StringNode()
+				mod_id: StringNode(),
 			},
 			'lithostitched:not': {
-				predicate: Reference(`${ID}:modifier_predicate`)
-			}
-		}
+				predicate: Reference(`${ID}:modifier_predicate`),
+			},
+		},
 	}, {
-		context: `${ID}.modifier_predicate`, disableSwitchContext: true
+		context: `${ID}.modifier_predicate`, disableSwitchContext: true,
 	}))
 }
