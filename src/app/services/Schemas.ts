0739a5b3ec3648cb5d8157ec986b0cd6723b6327
Misode <misoloo64@gmail.com>
2024-03-01 18:14:00 +0100

    Add 1.20.5

--- src/app/services/Schemas.ts
+++ src/app/services/Schemas.ts
@@ -1,11 +1,11 @@
 import type { CollectionRegistry, INode, SchemaRegistry } from '@mcschema/core'
 import { ChoiceNode, DataModel, Reference, StringNode } from '@mcschema/core'
 import config from '../Config.js'
-import { message } from '../Utils.js'
 import { initPartners } from '../partners/index.js'
+import { message } from '../Utils.js'
 import { fetchData } from './DataFetcher.js'
 
-export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4', '1.20', '1.20.2', '1.20.3'] as const
+export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4', '1.20', '1.20.2', '1.20.3', '1.20.5'] as const
 export type VersionId = typeof VersionIds[number]
 
 export const DEFAULT_VERSION: VersionId = '1.20.3'
@@ -51,6 +51,7 @@ const versionGetter: {
 	'1.20': () => import('@mcschema/java-1.20'),
 	'1.20.2': () => import('@mcschema/java-1.20.2'),
 	'1.20.3': () => import('@mcschema/java-1.20.3'),
+	'1.20.5': () => import('@mcschema/java-1.20.5'),
 }
 
 export let CachedDecorator: INode<any>
