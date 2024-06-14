e5a0248ccbcba36349ab75769b0b0f599f6346f0
Misode <misoloo64@gmail.com>
2024-06-14 05:52:46 +0200

    Refactor a bit

--- src/app/partners/index.ts
+++ src/app/partners/index.ts
@@ -1,5 +1,5 @@
 import type { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
-import { VersionId } from '../services/Schemas.js'
+import type { VersionId } from '../services/Schemas.js'
 import { initImmersiveWeathering } from './ImmersiveWeathering.js'
 import { initLithostitched } from './Lithostitched.js'
 import { initObsidian } from './Obsidian.js'
@@ -8,9 +8,9 @@ import { initOhTheTreesYoullGrow } from './OhTheTreesYoullGrow.js'
 export * from './ImmersiveWeathering.js'
 export * from './Lithostitched.js'
 
-export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry, id: VersionId) {
+export function initPartners(schemas: SchemaRegistry, collections: CollectionRegistry, version: VersionId) {
 	initImmersiveWeathering(schemas, collections)
-	initLithostitched(schemas, collections, id)
+	initLithostitched(schemas, collections, version)
 	initObsidian(schemas, collections)
 	initOhTheTreesYoullGrow(schemas, collections)
 }
