d29d612edb28bc804fd8e2b362df2c101ed9c174
Misode <misoloo64@gmail.com>
2024-12-28 00:46:39 +0100

    Fix dependency label on modded generator cards

--- src/app/components/generator/GeneratorCard.tsx
+++ src/app/components/generator/GeneratorCard.tsx
@@ -53,7 +53,7 @@ export function GeneratorCard({ id, minimal }: Props) {
 		return []
 	}, [gen])
 
-	return <Card title={<>{title}{icon && Icons[icon]}</>} overlay={gen.tags?.includes('partners') ? locale(`partner.${gen.id.substring(0, gen.id.indexOf('.'))}`) : versionText} link={cleanUrl(gen.url)}>
+	return <Card title={<>{title}{icon && Icons[icon]}</>} overlay={gen.dependency ? locale(`partner.${gen.dependency}`) : versionText} link={cleanUrl(gen.url)}>
 		{!gen.noPath && <p class="card-subtitle">/{gen.path ?? gen.id}</p>}
 		{tags.length > 0 && <div class="badges-list">
 			{tags.sort().map(tag => <Badge label={tag} />)}
