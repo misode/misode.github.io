108bc15cb2400bdb221b097bc6f5a34b120e806f
Misode <misoloo64@gmail.com>
2025-04-12 17:15:13 +0200

    Add 1.21.6 and set default to 1.21.5

--- src/app/services/Versions.ts
+++ src/app/services/Versions.ts
@@ -1,9 +1,9 @@
 import config from '../Config.js'
 
-export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4', '1.20', '1.20.2', '1.20.3', '1.20.5', '1.21', '1.21.2', '1.21.4', '1.21.5'] as const
+export const VersionIds = ['1.15', '1.16', '1.17', '1.18', '1.18.2', '1.19', '1.19.3', '1.19.4', '1.20', '1.20.2', '1.20.3', '1.20.5', '1.21', '1.21.2', '1.21.4', '1.21.5', '1.21.6'] as const
 export type VersionId = typeof VersionIds[number]
 
-export const DEFAULT_VERSION: VersionId = '1.21.4'
+export const DEFAULT_VERSION: VersionId = '1.21.5'
 
 export function checkVersion(versionId: string, minVersionId: string | undefined, maxVersionId?: string) {
 	const version = config.versions.findIndex(v => v.id === versionId)
